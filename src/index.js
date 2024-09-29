eeb7d585eb745eb1c5be2aa9479bfbe931bbfd92
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2024-09-29 17:38:34 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR before listening: ", error);
+      throw error;
+    });
+    
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`app is listening on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+  });
 
 /*
 const app = express()(async () => {
