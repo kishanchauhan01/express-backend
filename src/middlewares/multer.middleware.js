ad71faec3947e9a1dfc06564fc24b96377efb55c
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2024-10-04 21:53:15 +0530

    add multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,14 @@
-// import multer from "multer";
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname);
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage,
+});
