ca1f0202edcca13dca0ac124c781d81229725787
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2024-10-05 11:45:10 +0530

    all config setup

--- src/app.js
+++ src/app.js
@@ -13,8 +13,16 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
-    
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.route.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+//http://localhost:8000/api/v1/user/register
+
 
 export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,2 +1,9 @@
-import {asyncHandler} from '../utils/asyncHandler.js'
+import { asyncHandler } from "../utils/asyncHandler.js";
 
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "hello",
+  });
+});
+
+export { registerUser };
